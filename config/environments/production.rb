1a0b2e43a1de987a49251e468904f48c4fa9bb23
Alexander Ospina Castiblanco <ingaxl@gmail.com>
2012-09-14 00:59:45 -0500

    solucion emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Colombiandeveloper::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'colombiandeveloper.com' }
+  config.action_mailer.default_url_options = { :host => 'colombiandeveloper.herokuapp.com' }
     
   # Enable threaded mode
   # config.threadsafe!
