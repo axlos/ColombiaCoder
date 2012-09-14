d15b967ee40d81fbc6428ba03d323628770db73e
Alexander Ospina Castiblanco <ingaxl@gmail.com>
2012-09-14 03:09:47 -0500

    * se agrego validacion de url en contacto * informacion de privacidad en contacto * mejora en los skills en la oferta ya no carga todos

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,7 @@ class Contact < ActiveRecord::Base
   
   attr_accessible :company, :email, :full_name, :phone, :street_address, :web_site, :location
   
+  validates_format_of :web_site, :with => URI::regexp(%w(http https))
   validates :email, :email => true, :uniqueness => { :case_sensitive => false }
  
 end
