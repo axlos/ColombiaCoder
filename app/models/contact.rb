bd1896936dc76f4ff0eaa97a3685dcec88b1ae5b
Alexander Ospina Castiblanco <ingaxl@gmail.com>
2012-09-17 00:59:52 -0500

    Se modifican las imagenes del index solucion al error de autenticacion

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,6 @@ class Contact < ActiveRecord::Base
   
   attr_accessible :company, :email, :full_name, :phone, :street_address, :web_site, :location
   
-  validates_format_of :web_site, :with => URI::regexp(%w(http https))
   validates :email, :email => true, :uniqueness => { :case_sensitive => false }
  
 end
