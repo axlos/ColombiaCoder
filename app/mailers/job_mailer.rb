0156eb6b7787ab2d7dd5520bde562567a90c07fe
Alexander Ospina Castiblanco <ingaxl@gmail.com>
2012-09-28 23:08:27 -0500

    * Adjuntar archivos en s3

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -12,10 +12,13 @@ class JobMailer < ActionMailer::Base
     @seeker = seeker
     
     uri = Addressable::URI.parse(seeker.authenticated_url)
-    # UTL de documento almacenado en S3
-    response = Net::HTTP.start(uri.host, uri.port) { |http| http.get uri.path }
+    # URI de documento almacenado en S3
+    res = Net::HTTP.start(uri.host, uri.port) { |http| 
+      request = Net::HTTP::Get.new uri.request_uri
+      http.request request
+    }
     # adjuntar resume
-    attachments[seeker.resume_file_name] = response.body
+    attachments[seeker.resume_file_name] = res.body
     # enviar hoja de vida
     mail(:to => @job.email_address, :bcc => [@seeker.email, INFO_EMAIL], :subject => "Aspirante para #{@job.job_title}")
   end
--- app/models/seeker.rb
+++ app/models/seeker.rb
@@ -9,7 +9,6 @@ class Seeker < ActiveRecord::Base
   
   # archivo adjunto de hoja de vida en AWS S3
   has_attached_file :resume, :storage => :s3
-  #, :s3_credentials => { :bucket => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
   
   validates :resume, :attachment_presence => true
   validates_with AttachmentPresenceValidator, :attributes => :resume
